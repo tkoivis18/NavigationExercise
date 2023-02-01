363e80cc0c89436151a5afeedcc37ed821052c83
tkoivis18 <tobias.koivisto@gmail.com>
2023-02-01 16:27:52 +0200

    added MealItem

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,12 +1,46 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
+import { View, FlatList, Text, StyleSheet } from "react-native";
+import { MealItem } from "../components/MealItem";
 
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 
-export const MealsOverviewScreen = ({ route }) => {
+export const MealsOverviewScreen = ({ route, navigation }) => {
   const catId = route.params.categoryId;
+
+  //return true if mealItem.categoryIds has the category inside of it. catId checks the index of the item. If return -1 then false
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id == catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  const renderMealItem = (itemData) => {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
